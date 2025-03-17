aa8584d801bda652ef5d97c1743d21c8f65310e1
Jerome Pareja <parejajerome_bsit@plmun.edu.ph>
2025-03-17 21:50:56 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 01

--- VariablesAndCaseSensitivity.java
+++ VariablesAndCaseSensitivity.java
@@ -6,10 +6,10 @@ public class VariablesAndCaseSensitivity {
         int counter = 10;
         int Counter = 20;
 
-        System.out.println("Student Age:" + studentAge);
-        System.out.println("Item Price:$" + itemPrice);
-        System.out.println("Student Name:" + studentName);
-        System.out.println("Counter:" + counter);
-        System.out.println("Counter (Different Case):" + Counter);
+        System.out.println("Student Age :" + studentAge);
+        System.out.println("Item Price: $" + itemPrice);
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Counter: " + counter);
+        System.out.println("Counter (Different Case): " + Counter);
     }
 }
